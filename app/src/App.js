087f6e48ee72c99607f12cd6dd995983be374e9f
Eduardo Duarte <eduduarte.dev@gmail.com>
2020-09-03 00:51:01 -0300

    Utilizando render props em React

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,74 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
+import Counter from './Counter'
 
-const store = [
-  {
-    type: 'Roupa'
-  }, {
-    type: 'Calçado'
-  }, {
-    type: 'Camiseta'
-  }
-]
+// const store = [
+//   {
+//     type: 'Roupa'
+//   }, {
+//     type: 'Calçado'
+//   }, {
+//     type: 'Camiseta'
+//   }
+// ]
 
-function Column({type}) {
-  return (
-    <tr>
-      <td>{type.name.console}</td>
-    </tr>
-  )
-}
+// function Column({type}) {
+//   return (
+//     <tr>
+//       <td>{type.name.console}</td>
+//     </tr>
+//   )
+// }
 
-function App() {
+// function App() {
 
-  const renderColumns = (element,key) => (
-    <Fragment key={`column - ${key}`}>
-      <Column type={element.type} />
-    </Fragment>
+//   const renderColumns = (element,key) => (
+//     <Fragment key={`column - ${key}`}>
+//       <Column type={element.type} />
+//     </Fragment>
 
-  )
+//   )
     
 
-  return (
-    <table>
+//   return (
+//     <table>
       
-        {store.map(renderColumns)}
+//         {store.map(renderColumns)}
       
-    </table>
-  )
+//     </table>
+//   )
+// }
+
+const Buttons = ({ increment, decrement, count }) => (
+  <div>
+    <h1>Valor Atual: {count}</h1>
+    <div>
+      <button onClick={increment}>Adicionar 1</button>
+    </div>
+    <div>
+      <button onClick={decrement}>Remover 1</button>
+    </div>
+  </div>
+)
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Counter render={
+          ({ increment, decrement, count}) => (
+            <Buttons 
+              increment={increment}
+              decrement={decrement}
+              count={count}
+            />
+          )
+        }>
+
+        </Counter>
+      </div>
+    )
+  }
+
 }
 export default App
